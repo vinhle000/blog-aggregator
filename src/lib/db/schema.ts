1a2d519677ceb4a2cdbfd447a588f387f55e849b
vinhle000 <vinhle000@gmail.com>
2025-10-01 13:48:32 -0700

    fixed user schema typos

--- src/lib/db/schema.ts
+++ src/lib/db/schema.ts
@@ -2,7 +2,7 @@ import { pgTable, timestamp, uuid, text } from 'drizzle-orm/pg-core';
 
 export const users = pgTable('users', {
   id: uuid('uuid').primaryKey().defaultRandom().notNull(),
-  createdAt: timestamp('createdPat').notNull(),
+  createdAt: timestamp('created_at').notNull().defaultNow(),
   updatedAt: timestamp('updated_at')
     .notNull()
     .defaultNow()
