2021ace93d5aef7769769149765a7ed8209f35c3
vinhle000 <vinhle000@gmail.com>
2025-09-30 19:03:57 -0700

    added CommandHandler and CommandRegistry types. Implemented registering commands to call specific handler function from input arguments

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,55 @@
-import { setUser, readConfig } from '../config.js';
+import { setUser, readConfig } from './config.js';
+
+type CommandHandler = (cmdName: string, ...args: string[]) => void;
+
+// Allows additional fields of type CommandHandler
+// through index signature
+// type CommandRegistry = {
+//   [key: string]: CommandHandler;
+// };
+
+// through Record utility
+type CommandRegistry = Record<string, CommandHandler>;
+
+const commands: CommandRegistry = {};
+
+// This function registers a new handler function for a command name.
+function registerCommand(
+  registry: CommandRegistry,
+  cmdName: string,
+  handler: CommandHandler
+) {}
+
+// This function runs a given command with the provided state if it exists.
+function runCommand(
+  registry: CommandRegistry,
+  cmdName: string,
+  ...args: string[]
+) {}
+
+function handlerLogin(cmdName: string, ...args: string[]): void {
+  if (!args || args.length === 0) {
+    throw new Error(`username is required for login`);
+  }
+  const username = args[0];
+
+  setUser(username);
+  console.log(`user has been set to ${username}`);
+}
 
 function main() {
-  setUser('Vinh');
-  const config = readConfig();
+  // register commands
+  const commands: Record<string, CommandHandler> = {} as CommandRegistry;
+  commands['login'] = handlerLogin;
 
-  for (const entry of Object.entries(config)) {
-    console.log(JSON.stringify(entry));
+  const inputArgs: string[] = process.argv.slice(2);
+  if (inputArgs.length === 0) {
+    throw new Error('Not enough arguments were provided');
+  }
+
+  const [cmdName, ...args] = inputArgs;
+  if (cmdName in commands) {
+    commands[cmdName](cmdName, ...args);
   }
 }
 
